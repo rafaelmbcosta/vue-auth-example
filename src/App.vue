8ed3a5a4ba3a725a9e7985e0a3a10eb6ae7214c9
Rafael Costa <rafael.costa@pge.ce.gov.br>
2018-09-24 09:26:56 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <app-token> </app-token>
+  </div>
+</template>
+
+<script>
+import Token from './components/Token.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-token': Token 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
